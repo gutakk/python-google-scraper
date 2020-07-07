e1ee261f215b5ac5409e6bf374d5644b44032609
Gut <thiramate.dev@gmail.com>
2020-07-07 20:43:53 +0700

    init register page

---
+++ web/src/pages/register/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>hello nimble</div>
+        )
+    }
+}
+
+export default Register
