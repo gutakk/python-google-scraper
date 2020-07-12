0a462e6123c71bd0433a9890df673d0f7cb2770b
Gut <thiramate.dev@gmail.com>
2020-07-12 22:21:19 +0700

    add history

--- web/__tests__/actions/registerAction.test.js
+++ web/__tests__/actions/registerAction.test.js
@@ -1,10 +1,11 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 import * as actions from '../../src/redux/actions/registerAction'
+import fetchMock, { mock } from 'fetch-mock'
 
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares);
-const store = mockStore();
+let store = mockStore();
 
 
 describe("clear action", () => {
@@ -51,15 +52,40 @@ describe("clear action", () => {
         })
     })
 
-    describe("confirm password change", () => {
-        test("Dispatches the correct action and payload", () => {
+    describe("register click", () => {
+        test("Password and Confirm password not match", () => {
+            
             const expectedActions = [
-                {
-                    "payload": "1234",
-                    "type": "register/CONFIRM_PASSWORD_CHANGE",
-                }
+                {"type": "register/REGISTER_CLICK"},
+                {"type": "register/REGISTERING"},
+                {"type": "register/PASSWORD_NOT_MATCH"}
             ]
-            store.dispatch(actions.onConfirmPasswordChanged("1234"));
+            store = mockStore({
+                register: {
+                    email: "test@email.com",
+                    password: "1234",
+                    confirmPassword: "123"
+                }
+            })
+            store.dispatch(actions.onRegisterClicked());
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+
+        test("Password and Confirm password match", () => {
+            
+            const expectedActions = [
+                {"type": "register/REGISTER_CLICK"},
+                {"type": "register/REGISTERING"},
+                {"type": "register/PASSWORD_NOT_MATCH"}
+            ]
+            store = mockStore({
+                register: {
+                    email: "test@email.com",
+                    password: "1234",
+                    confirmPassword: "1234"
+                }
+            })
+            store.dispatch(actions.onRegisterClicked());
             expect(store.getActions()).toEqual(expectedActions);
         })
     })
