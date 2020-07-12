dc45b14c7991df0a12589166d5f777a49737d09a
Gut <thiramate.dev@gmail.com>
2020-07-13 01:58:05 +0700

    add retry if task failed

--- backend/worker.py
+++ backend/worker.py
@@ -8,38 +8,41 @@ from utils import app, init_cnx
 client = Celery(app.name, broker=os.environ["CELERY_BROKER_URL"])
 client.conf.update(app.config)
 
-@client.task
+@client.task(default_retry_delay=60, max_retries=120)
 def scrape_data_from_google(file_id, keyword):
     try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.get(f"https://www.google.com/search?q={keyword}")
-        content = driver.page_source
-        soup = BeautifulSoup(content, "html.parser")
+        try:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome(chrome_options=chrome_options)
+            driver.get(f"https://www.google.com/search?q={keyword}")
+            content = driver.page_source
+            soup = BeautifulSoup(content, "html.parser")
 
-        total_adword = count_adword(soup)
-        total_link = count_link(soup)
-        total_search_result = get_total_search_result(soup)
-    finally:
-        driver.close()
+            total_adword = count_adword(soup)
+            total_link = count_link(soup)
+            total_search_result = get_total_search_result(soup)
+        finally:
+            driver.close()
 
-    cnx = init_cnx()
-    cur = cnx.cursor()
-    try:
-        cur.execute("""
-            INSERT INTO data (file_id, keyword, total_adword, total_link, total_search_result, html_code)
-            VALUES (%s, %s, %s, %s, %s, %s);
-        """, [file_id, keyword, total_adword, total_link, total_search_result, soup.prettify()])
-        cnx.commit()
-    except Exception as e:
-        cnx.rollback()
-        raise(e)
-    finally:
-        cur.close()
-        cnx.close()
+        cnx = init_cnx()
+        cur = cnx.cursor()
+        try:
+            cur.execute("""
+                INSERT INTO data (file_id, keyword, total_adword, total_link, total_search_result, html_code)
+                VALUES (%s, %s, %s, %s, %s, %s);
+            """, [file_id, keyword, total_adword, total_link, total_search_result, soup.prettify()])
+            cnx.commit()
+        except Exception as e:
+            cnx.rollback()
+            raise(e)
+        finally:
+            cur.close()
+            cnx.close()
+    except SMTPException as e:
+       self.retry(exc=e)
 
 
 def count_adword(soup):
