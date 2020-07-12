6d8df38bc7a03ef5535fd1ecb34b661bbd8a110f
Gut <thiramate.dev@gmail.com>
2020-07-12 20:18:53 +0700

    setup frontend unit test

---
+++ web/__tests__/actions/registerAction.test.js
@@ -0,0 +1,66 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk'
+import * as actions from '../../src/redux/actions/registerAction'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares);
+const store = mockStore();
+
+
+describe("clear action", () => {
+    beforeEach(() => { // Runs before each test in the suite
+        store.clearActions();
+    })
+
+    describe("email change", () => {
+        test("Dispatches the correct action and payload", () => {
+            const expectedActions = [
+                {
+                    "payload": "test@email.com",
+                    "type": "register/EMAIL_CHANGE",
+                }
+            ]
+            store.dispatch(actions.onEmailChanged("test@email.com"));
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+    })
+
+    describe("password change", () => {
+        test("Dispatches the correct action and payload", () => {
+            const expectedActions = [
+                {
+                    "payload": "1234",
+                    "type": "register/PASSWORD_CHANGE",
+                }
+            ]
+            store.dispatch(actions.onPasswordChanged("1234"));
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+    })
+
+    describe("confirm password change", () => {
+        test("Dispatches the correct action and payload", () => {
+            const expectedActions = [
+                {
+                    "payload": "1234",
+                    "type": "register/CONFIRM_PASSWORD_CHANGE",
+                }
+            ]
+            store.dispatch(actions.onConfirmPasswordChanged("1234"));
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+    })
+
+    describe("confirm password change", () => {
+        test("Dispatches the correct action and payload", () => {
+            const expectedActions = [
+                {
+                    "payload": "1234",
+                    "type": "register/CONFIRM_PASSWORD_CHANGE",
+                }
+            ]
+            store.dispatch(actions.onConfirmPasswordChanged("1234"));
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+    })
+})
