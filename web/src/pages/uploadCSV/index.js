4276961e742df91853db2ded97dcb6f2a7f907dc
Gut <thiramate.dev@gmail.com>
2020-07-14 23:50:50 +0700

    check if not login then render require login page first

--- web/src/pages/uploadCSV/index.js
+++ web/src/pages/uploadCSV/index.js
@@ -25,68 +25,75 @@ class UploadCSV extends Component {
             csvMoreThan100KW
         } = this.props
         return (
-            <div id="upload-csv-container" className="d-flex flex-column align-items-center">
-                <div className="custom-file">
-                    <input type="file" className="custom-file-input" id="customFile" onChange={onUpload}/>
-                    <label className="custom-file-label">Choose file</label>
-                </div>
-                <div id="refresh-btn-container"><button className="btn btn-primary" onClick={fetchCSVAction}>Refresh</button></div>
+            <div id="upload-csv-container">
                 {
-                    isFetching ?
-                    <div className="spinner"><i className="fa fa-circle-o-notch fa-spin display-1"></i></div>
-                    :
-                    csvList.length > 0 ?
-                    <table className="table table-bordered">
-                        <thead>
-                            <tr className="text-center">
-                            <th scope="col">Status</th>
-                            <th scope="col">File name</th>
-                            <th scope="col">Keywords    </th>
-                            <th scope="col">Uploaded</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {csvList.map(csv => {
-                                return (
-                                    <tr key={csv.fileId} className="text-center">
-                                        <td>
-                                            {
-                                                csv.status ?
-                                                <Link to={`/data-report/${csv.fileId}`}>
-                                                    <button><i className="fa fa-eye"></i></button>
-                                                </Link>
-                                                :
-                                                <button className="btn btn-primary" disabled>PROCESSING</button>
-                                            }
-                                        </td>
-                                        <td>{csv.filename}</td>
-                                        <td>{csv.keywords}</td>
-                                        <td>{csv.created}</td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                    :
-                    <h2 className="text-muted">NO UPLOADED FILES</h2>
-                }
-                {
-                    csvMoreThan100KW &&
-                    <div className="modal fade show d-block" tabindex="-1" role="dialog">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">CSV Contains more than 100 keywords</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={closeMoreThan100KWModal}>
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={closeMoreThan100KWModal}>Close</button>
-                            </div>
-                            </div>
+                    localStorage.getItem("token") ?
+                    <div className="d-flex flex-column align-items-center">
+                        <div className="custom-file">
+                            <input type="file" className="custom-file-input" id="customFile" onChange={onUpload}/>
+                            <label className="custom-file-label">Choose file</label>
                         </div>
+                        <div id="refresh-btn-container"><button className="btn btn-primary" onClick={fetchCSVAction}>Refresh</button></div>
+                        {
+                            isFetching ?
+                            <div className="spinner"><i className="fa fa-circle-o-notch fa-spin display-1"></i></div>
+                            :
+                            csvList.length > 0 ?
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr className="text-center">
+                                    <th scope="col">Status</th>
+                                    <th scope="col">File name</th>
+                                    <th scope="col">Keywords    </th>
+                                    <th scope="col">Uploaded</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {csvList.map(csv => {
+                                        return (
+                                            <tr key={csv.fileId} className="text-center">
+                                                <td>
+                                                    {
+                                                        csv.status ?
+                                                        <Link to={`/data-report/${csv.fileId}`}>
+                                                            <button><i className="fa fa-eye"></i></button>
+                                                        </Link>
+                                                        :
+                                                        <button className="btn btn-primary" disabled>PROCESSING</button>
+                                                    }
+                                                </td>
+                                                <td>{csv.filename}</td>
+                                                <td>{csv.keywords}</td>
+                                                <td>{csv.created}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                            :
+                            <h2 className="text-muted">NO UPLOADED FILES</h2>
+                        }
+                        {
+                            csvMoreThan100KW &&
+                            <div className="modal fade show d-block" tabindex="-1" role="dialog">
+                                <div className="modal-dialog" role="document">
+                                    <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title">CSV Contains more than 100 keywords</h5>
+                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={closeMoreThan100KWModal}>
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={closeMoreThan100KWModal}>Close</button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        }
                     </div>
+                    :
+                    <div id="require-login" className="text-center"><h2 className="text-muted">This application require to login first</h2></div>
                 }
             </div>
         )
