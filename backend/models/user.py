6fef9656db999cef935f6c3450ce0243e04a64f3
Gut <thiramate.dev@gmail.com>
2020-07-17 16:26:08 +0700

    add specific index

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Index
 from database import Base
 
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False, index=True, unique=True)
-    password = Column(String, nullable=False, index=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    __table_args__ = (Index('email_password_idx', 'email', 'password'),)
