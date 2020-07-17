9ecb8f0fdb7d86117c889f827840a05a30fe7698
Gut <thiramate.dev@gmail.com>
2020-07-17 16:20:21 +0700

    add id to data table

--- backend/models/data.py
+++ backend/models/data.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.sql import func
 from database import Base
 import models.file
@@ -6,10 +6,12 @@ import models.file
 
 class Data(Base):
     __tablename__ = "data"
-    file_id = Column(Integer, ForeignKey("file.id"), primary_key=True)
-    keyword = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    file_id = Column(Integer, ForeignKey("file.id"), nullable=False)
+    keyword = Column(String, nullable=False)
     total_adword = Column(Integer)
     total_link = Column(Integer)
     total_search_result = Column(String)
     html_code = Column(String)
     created = Column(DateTime, server_default=func.now())
+    __table_args__ = (UniqueConstraint('file_id', 'keyword'),)
